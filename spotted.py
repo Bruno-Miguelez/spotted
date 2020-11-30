8857bda6ae074a2b1b9007850b3ca2c2d7502195
apatacadof <62858402+apatacadof@users.noreply.github.com>
2020-11-30 01:23:40 -0300

    Create spotted.py

---
+++ spotted.py
@@ -0,0 +1,19 @@
+import tweepy
+import time
+import random
+
+auth = tweepy.OAuthHandler()
+auth.set_access_token()
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+messages = api.list_direct_messages()
+for message in messages:
+        try:
+            print (message.message_create['message_data']['text'])
+            text = message.message_create["message_data"]["text"]
+            api.update_status(f'spotted: {text}')
+            # remove DM
+            api.destroy_direct_message(message.id)
+else:
+        print ("nada bro")
+        time.sleep(300)
